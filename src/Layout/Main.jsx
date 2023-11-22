01cfa25c8e5c61e65f3d33f17b781799696ac500
smhabib99-99 <habibaust34@gmail.com>
2023-11-22 20:05:19 +0600

    creating Login and validate captcha is successful

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,15 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../pages/shared/Footer/Footer";
 import Navbar from "../pages/shared/Navbar/Navbar";
 
 
 const Main = () => {
+    const location = useLocation();
+    console.log(location);
+    const noHeaderFooter = location.pathname.includes('login')
     return (
         <div>
             {/* <h2>This is main</h2> */}
-            <Navbar></Navbar>
+            {noHeaderFooter || <Navbar></Navbar>}
             <Outlet></Outlet>
-            <Footer></Footer>
+            {noHeaderFooter || <Footer></Footer>}
         </div>
     );
 };
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import Main from "../Layout/Main";
 import Home from "../pages/Home/Home";
 import Menu from "../pages/Menu/Menu/Menu";
 import Order from "../pages/Order/Order/Order";
+import Login from "../pages/Login/Login";
 
 
 
@@ -22,6 +23,10 @@ export const router = createBrowserRouter([
         {
           path: 'order/:category',
           element:<Order></Order>
+        },
+        {
+          path: 'login',
+          element:<Login></Login>
         }
       ]
     },
